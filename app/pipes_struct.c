2d328a4ada38878fe3155a0a8f623804c5ff1329
sergio johann filho <sergio.johann@acad.pucrs.br>
2021-10-26 10:45:49 -0300

    added pipes_struct example

---
+++ app/pipes_struct.c
@@ -0,0 +1,59 @@
+#include <ucx.h>
+
+struct pipe_s *pipe1;
+
+struct data1_s {
+	char v[20];
+	int32_t a;
+	int16_t b;
+};
+
+void task1(void)
+{
+	char data1[64];
+	int32_t i = 0;
+	struct data1_s *ptr = (struct data1_s *)&data1;
+	char guard[256];
+
+	ucx_task_init(guard, sizeof(guard));
+
+	ptr->a = 12345;
+	ptr->b = -555;
+
+	while (1) {
+		_sprintf(ptr->v, "hello %d", i++);
+		ptr->a++;
+		ptr->b++;
+
+		ucx_pipe_write(pipe1, data1, sizeof(struct data1_s));
+		
+		_delay_ms(500);
+	}
+}
+
+void task0(void)
+{
+	char data[64];
+	uint16_t s;
+	struct data1_s *ptr = (struct data1_s *)&data;
+	char guard[256];
+
+	ucx_task_init(guard, sizeof(guard));
+
+	while (1) {
+		while (ucx_pipe_size(pipe1) < sizeof(struct data1_s));
+		s = ucx_pipe_read(pipe1, data, ucx_pipe_size(pipe1));
+		_printf("pipe (%d): %s %d %d\n", s, ptr->v, ptr->a, ptr->b);
+	}
+}
+
+int32_t app_main(void)
+{
+	ucx_task_add(task0);
+	ucx_task_add(task1);
+
+	pipe1 = ucx_pipe_create(64);		/* pipe buffer, 64 bytes - powers of two */
+
+	// start UCX/OS, preemptive mode
+	return 1;
+}
