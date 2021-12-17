84bcd7ed1f3bd1ab4f2b80dbbc7ca69c840195f1
sergio johann filho <sergio.johann@acad.pucrs.br>
2021-12-17 19:50:14 -0300

    preliminary RV64 / LLVM target

---
+++ arch/riscv/riscv64-qemu-llvm/arch.mak
@@ -0,0 +1,29 @@
+# this is stuff specific to this architecture
+ARCH_DIR = $(SRC_DIR)/arch/$(ARCH)
+INC_DIRS  = -I $(ARCH_DIR)
+
+F_CLK=10000000
+
+#remove unreferenced functions
+CFLAGS_STRIP = -fdata-sections -ffunction-sections
+LDFLAGS_STRIP = --gc-sections
+
+# this is stuff used everywhere - compiler and flags should be declared (ASFLAGS, CFLAGS, LDFLAGS, LD_SCRIPT, CC, AS, LD, DUMP, READ, OBJ and SIZE).
+ASFLAGS = -march=rv64i -mabi=lp64 #-fPIC
+CFLAGS = -Wall --target=riscv64 -march=rv64im -mabi=lp64 -O2 -c -ffreestanding -nostdlib -fomit-frame-pointer -mcmodel=medany $(INC_DIRS) -DCPU_SPEED=${F_CLK} -DLITTLE_ENDIAN $(CFLAGS_STRIP) -DTERM_BAUD=$(SERIAL_BAUD)
+
+LDFLAGS = -melf64lriscv $(LDFLAGS_STRIP)
+LDSCRIPT = $(ARCH_DIR)/riscv64-qemu.ld
+
+CC = clang
+AS = riscv64-unknown-elf-as
+LD = riscv64-unknown-elf-ld
+DUMP = riscv64-unknown-elf-objdump -Mno-aliases
+READ = riscv64-unknown-elf-readelf
+OBJ = riscv64-unknown-elf-objcopy
+SIZE = riscv64-unknown-elf-size
+
+hal:
+	$(AS) $(ASFLAGS) -o crt0.o $(ARCH_DIR)/crt0.s
+	$(CC) $(CFLAGS) \
+		$(ARCH_DIR)/hal.c 
---
+++ arch/riscv/riscv64-qemu-llvm/hal.c
@@ -0,0 +1,180 @@
+/* file:          hal.c
+ * description:   hardware abstraction layer for RISC-V (64 bits)
+ * date:          05/2021
+ * author:        Sergio Johann Filho <sergio.johann@acad.pucrs.br>
+ */
+
+#include <hal.h>
+#include <libc.h>
+
+/* hardware platform dependent stuff */
+void _putchar(char value)		// polled putchar()
+{
+	while (!((NS16550A_UART0_CTRL_ADDR(NS16550A_LSR) & NS16550A_LSR_RI)));
+	NS16550A_UART0_CTRL_ADDR(NS16550A_THR) = value;
+}
+
+int32_t _kbhit(void)
+{
+	return 0;
+}
+
+int32_t _getchar(void)
+{
+	return 0;
+}
+
+int32_t _interrupt_set(int32_t s)
+{
+	volatile int32_t val;
+	
+	val = read_csr(mstatus) & 0x8;
+	if (s) {
+		asm volatile ("csrs mstatus, 8");
+	} else {
+		asm volatile ("csrc mstatus, 8");
+	}
+
+	return val;
+}
+
+void _delay_ms(uint32_t msec)
+{
+	volatile uint32_t cur, last, delta, msecs;
+	uint32_t cycles_per_msec;
+
+	last = MTIME;
+	delta = msecs = 0;
+	cycles_per_msec = CPU_SPEED / 1000;
+	while (msec > msecs) {
+		cur = MTIME;
+		if (cur < last)
+			delta += (cur + (CPU_SPEED - last));
+		else
+			delta += (cur - last);
+		last = cur;
+		if (delta >= cycles_per_msec) {
+			msecs += delta / cycles_per_msec;
+			delta %= cycles_per_msec;
+		}
+	}
+}
+
+void _delay_us(uint32_t usec)
+{
+	volatile uint32_t cur, last, delta, usecs;
+	uint32_t cycles_per_usec;
+
+	last = MTIME;
+	delta = usecs = 0;
+	cycles_per_usec = CPU_SPEED / 1000000;
+	while (usec > usecs) {
+		cur = MTIME;
+		if (cur < last)
+			delta += (cur + (CPU_SPEED - last));
+		else
+			delta += (cur - last);
+		last = cur;
+		if (delta >= cycles_per_usec) {
+			usecs += delta / cycles_per_usec;
+			delta %= cycles_per_usec;
+		}
+	}
+}
+
+static void uart_init(uint32_t baud)
+{
+	uint32_t divisor = CPU_SPEED / (16 * baud);
+
+	NS16550A_UART0_CTRL_ADDR(NS16550A_LCR) = NS16550A_LCR_DLAB;
+	NS16550A_UART0_CTRL_ADDR(NS16550A_DLM) = (divisor >> 8) & 0xff;
+	NS16550A_UART0_CTRL_ADDR(NS16550A_DLL) = divisor & 0xff;
+	NS16550A_UART0_CTRL_ADDR(NS16550A_LCR) = NS16550A_LCR_8BIT;
+}
+
+void _cpu_idle(void)
+{
+	asm volatile ("wfi");
+}
+
+void _irq_handler(uint32_t cause, uint32_t *stack)
+{
+	uint32_t val;
+	
+	val = read_csr(mcause);
+	if (mtime_r() > mtimecmp_r()) {
+		mtimecmp_w(mtime_r() + 0x1ffff);
+		dispatcher();
+	} else {
+		_printf("[%x]\n", val);
+		for (;;);
+	}
+
+}
+
+uint32_t _readcounter(void)
+{
+	return MTIME_L;
+}
+
+uint64_t _read_us(void)
+{
+	static uint64_t timeref = 0;
+	static uint32_t tval2 = 0, tref = 0;
+
+	if (tref == 0) _readcounter();
+	if (_readcounter() < tref) tval2++;
+	tref = _readcounter();
+	timeref = ((uint64_t)tval2 << 32) + (uint64_t)_readcounter();
+
+	return (timeref / (CPU_SPEED / 1000000));
+}
+
+uint64_t mtime_r(void)
+{
+	return MTIME;
+}
+
+void mtime_w(uint64_t val)
+{
+	MTIME = val;
+}
+
+uint64_t mtimecmp_r(void)
+{
+	return MTIMECMP;
+}
+
+void mtimecmp_w(uint64_t val)
+{
+	MTIMECMP = val;
+}
+
+void _panic(void)
+{
+	volatile int * const exit_device = (int* const)0x100000;
+	*exit_device = 0x5555;
+	while (1);
+}
+
+void _hardware_init(void)
+{
+	uart_init(TERM_BAUD);
+	mtimecmp_w(mtime_r() + 0x1ffff);
+	write_csr(mie, 128);
+}
+
+void _timer_enable(void)
+{
+	asm volatile ("csrs mstatus, 8");
+}
+
+void _timer_disable(void)
+{
+	asm volatile ("csrc mstatus, 8");
+}
+
+void _interrupt_tick(void)
+{
+	_ei(1);
+}
