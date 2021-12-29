a750d379227c816086ae7cd71cf72f7994715db8
Sergio Johann Filho <sergio.johann@acad.pucrs.br>
2021-12-29 19:25:30 -0300

    fix possible uninitialized variable access

--- arch/riscv/riscv32-qemu/hal.c
+++ arch/riscv/riscv32-qemu/hal.c
@@ -239,7 +239,7 @@ uint64_t __udivmoddi4(uint64_t num, uint64_t den, uint64_t *rem_p){
 }
 
 uint64_t __umoddi3(uint64_t num, uint64_t den){
-	uint64_t v;
+	uint64_t v = 0;
 
 	(void) __udivmoddi4(num, den, &v);
 	return v;
@@ -251,7 +251,7 @@ uint64_t __udivdi3(uint64_t num, uint64_t den){
 
 int64_t __moddi3(int64_t num, int64_t den){
 	int minus = 0;
-	int64_t v;
+	int64_t v = 0;
 
 	if (num < 0){
 		num = -num;
