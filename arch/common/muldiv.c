3bfb6ed9d84bc098cad32182de393dc0c7f90cf1
sergio johann filho <sergio.johann@acad.pucrs.br>
2023-05-14 23:36:57 -0300

    improve mul/div software routines

--- arch/common/muldiv.c
+++ arch/common/muldiv.c
@@ -185,7 +185,7 @@ uint64_t __lshrdi3(uint64_t v, int32_t cnt)
 	return ((uint64_t)vh << 32) + vl;
 }
 
-uint64_t __udivmoddi4(uint64_t num, uint64_t den, int8_t mod)
+uint64_t __udivmoddi4(uint64_t num, uint64_t den, uint64_t *rem)
 {
 	uint64_t quot = 0, qbit = 1;
 
@@ -206,15 +206,45 @@ uint64_t __udivmoddi4(uint64_t num, uint64_t den, int8_t mod)
 		qbit >>= 1;
 	}
 
-	if (mod)
-		return num;
+	if (rem)
+		*rem = num;
 
 	return quot;
 }
 
+uint64_t __divmoddi4(int64_t num, int64_t den, int64_t *rem)
+{
+	int32_t neg = 0;
+	int64_t v;
+
+	if (num < 0) {
+		num = -num;
+		neg = 1;
+	}
+	
+	if (den < 0) {
+		den = -den;
+		neg ^= 1;
+	}
+
+	v = __udivmoddi4(num, den, (uint64_t *)rem);
+	
+	if (neg) {
+		v = -v;
+		if (rem)
+			*rem = -(*rem);
+	}
+	
+	return v;
+}
+
 uint64_t __umoddi3(uint64_t num, uint64_t den)
 {
-	return __udivmoddi4(num, den, 1);
+	uint64_t v = 0;
+	
+	__udivmoddi4(num, den, &v);
+	
+	return v;
 }
 
 uint64_t __udivdi3(uint64_t num, uint64_t den)
@@ -224,46 +254,14 @@ uint64_t __udivdi3(uint64_t num, uint64_t den)
 
 int64_t __moddi3(int64_t num, int64_t den)
 {
-	int32_t neg = 0;
 	int64_t v;
-
-	if (num < 0) {
-		num = -num;
-		neg = 1;
-	}
+ 
+	__divmoddi4(num, den, &v);
 	
-	if (den < 0) {
-		den = -den;
-		neg ^= 1;
-	}
-
-	v = __udivmoddi4(num, den, 1);
-	
-	if (neg)
-		v = -v;
-
 	return v;
 }
 
 int64_t __divdi3(int64_t num, int64_t den)
 {
-	int32_t neg = 0;
-	int64_t v;
-
-	if (num < 0) {
-		num = -num;
-		neg = 1;
-	}
-	
-	if (den < 0) {
-		den = -den;
-		neg ^= 1;
-	}
-
-	v = __udivmoddi4(num, den, 0);
-	
-	if (neg)
-		v = -v;
-
-	return v;
+	return __divmoddi4(num, den, 0);
 }
