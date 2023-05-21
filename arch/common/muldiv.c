b692ff3f1d95ce98c227b1222f042015ac9cbb12
sergio johann filho <sergio.johann@acad.pucrs.br>
2023-05-21 15:18:59 -0300

    important changes on generic mul/div routines to support ARM EABI

--- arch/common/muldiv.c
+++ arch/common/muldiv.c
@@ -78,7 +78,7 @@ uint32_t __udivmodsi4(uint32_t num, uint32_t den, int8_t mod)
 	return quot;
 }
 
-int32_t __divsi3(int32_t num, int32_t den)
+int32_t __divmodsi4(int32_t num, int32_t den, int8_t mod)
 {
 	int32_t neg = 0;
 	int32_t v;
@@ -93,34 +93,11 @@ int32_t __divsi3(int32_t num, int32_t den)
 		neg ^= 1;
 	}
 
-	v = __udivmodsi4(num, den, 0);
+	v = __udivmodsi4(num, den, mod);
 	
 	if (neg)
 		v = -v;
-
-	return v;
-}
-
-int32_t __modsi3(int32_t num, int32_t den)
-{
-	int32_t neg = 0;
-	int32_t v;
-
-	if (num < 0) {
-		num = -num;
-		neg = 1;
-	}
 	
-	if (den < 0) {
-		den = -den;
-		neg ^= 1;
-	}
-
-	v = __udivmodsi4(num, den, 1);
-	
-	if (neg)
-		v = -v;
-
 	return v;
 }
 
@@ -132,6 +109,16 @@ uint32_t __udivsi3(uint32_t num, uint32_t den)
 uint32_t __umodsi3(uint32_t num, uint32_t den)
 {
 	return __udivmodsi4(num, den, 1);
+}
+
+int32_t __divsi3(int32_t num, int32_t den)
+{
+	return __divmodsi4(num, den, 0);
+}
+
+int32_t __modsi3(int32_t num, int32_t den)
+{
+	return __divmodsi4(num, den, 1);
 }
 
 uint64_t __ashldi3(uint64_t v, int32_t cnt)
@@ -212,7 +199,7 @@ uint64_t __udivmoddi4(uint64_t num, uint64_t den, uint64_t *rem)
 	return quot;
 }
 
-uint64_t __divmoddi4(int64_t num, int64_t den, int64_t *rem)
+int64_t __divmoddi4(int64_t num, int64_t den, int64_t *rem)
 {
 	int32_t neg = 0;
 	int64_t v;
