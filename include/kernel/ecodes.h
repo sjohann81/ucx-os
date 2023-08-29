d2244241fa75165daaec7fb4ceab672c2eca3812
sergio johann filho <sergio.johann@acad.pucrs.br>
2023-08-29 11:16:00 -0300

    added error code sources

---
+++ include/kernel/ecodes.h
@@ -0,0 +1,25 @@
+enum ecodes {
+	ERR_OK = 0,
+	ERR_FAIL = -1,
+	ERR_NO_TASKS = 0x80000001,
+	ERR_KCB_ALLOC,
+	ERR_TCB_ALLOC,
+	ERR_STACK_ALLOC,
+	ERR_TASK_NOT_FOUND,
+	ERR_TASK_CANT_SUSPEND,
+	ERR_TASK_CANT_RESUME,
+	ERR_TASK_INVALID_PRIO,
+	ERR_STACK_CHECK,
+	ERR_PIPE_ALLOC,
+	ERR_PIPE_DEALLOC,
+	ERR_SEM_ALLOC,
+	ERR_SEM_DEALLOC,
+	ERR_UNKNOWN
+};
+
+struct ecode_s {
+	int32_t ecode;
+	char * const desc;
+};
+
+extern struct ecode_s *perror;
---
+++ kernel/ecodes.c
@@ -0,0 +1,22 @@
+#include <ucx.h>
+
+struct ecode_s edatadesc[] = {
+	{ERR_OK,			"no error"},
+	{ERR_FAIL,			"failure"},
+	{ERR_NO_TASKS,			"no tasks to run"},
+	{ERR_KCB_ALLOC,			"kcb alloc failed"},
+	{ERR_TCB_ALLOC,			"tcb alloc failed"},
+	{ERR_STACK_ALLOC,		"stack alloc failed"},
+	{ERR_TASK_NOT_FOUND,		"task not found"},
+	{ERR_TASK_CANT_SUSPEND,		"task suspend failed"},
+	{ERR_TASK_CANT_RESUME,		"task resume failed"},
+	{ERR_TASK_INVALID_PRIO,		"task invalid priority"},
+	{ERR_STACK_CHECK,		"stack check failed"},
+	{ERR_PIPE_ALLOC,		"pipe alloc failed"},
+	{ERR_PIPE_DEALLOC,		"pipe dealloc failed"},
+	{ERR_SEM_ALLOC,			"sema alloc failed"},
+	{ERR_SEM_DEALLOC,		"sema dealloc failed"},
+	{ERR_UNKNOWN,			"unknown reason"}
+};
+
+struct ecode_s *perror = edatadesc;
