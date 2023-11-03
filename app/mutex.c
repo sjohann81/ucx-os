049d0e002d7271ff653cee040d6f3f1388ba4ab5
sergio johann filho <sergio.johann@acad.pucrs.br>
2023-11-03 00:32:25 -0300

    update examples to new semaphore API

--- app/mutex.c
+++ app/mutex.c
@@ -5,20 +5,20 @@ struct sem_s *mutex;
 void task_a(void)
 {
 	for (;;) {
-		ucx_wait(mutex);
+		ucx_sem_wait(mutex);
 		printf("hello from task A, id %d\n", ucx_task_id());
 		printf("this is still task A!\n");
-		ucx_signal(mutex);
+		ucx_sem_signal(mutex);
 	}
 }
 
 void task_b(void)
 {
 	for (;;) {
-		ucx_wait(mutex);
+		ucx_sem_wait(mutex);
 		printf("hello from task B, id %d\n", ucx_task_id());
 		printf("this is still task B!\n");
-		ucx_signal(mutex);
+		ucx_sem_signal(mutex);
 	}
 }
 
@@ -27,7 +27,7 @@ int32_t app_main(void)
 	ucx_task_add(task_a, DEFAULT_STACK_SIZE);
 	ucx_task_add(task_b, DEFAULT_STACK_SIZE);
 
-	mutex = ucx_semcreate(1);
+	mutex = ucx_sem_create(2, 1);
 	
 	return 1;
 }
--- app/prodcons.c
+++ app/prodcons.c
@@ -11,13 +11,13 @@ void producer(void)
 
 	for (;;) {
 		item = random();
-		ucx_wait(empty);
-		ucx_wait(mutex);
+		ucx_sem_wait(empty);
+		ucx_sem_wait(mutex);
 		buffer[in] = item;
 		printf("\nproducer %d putting at %ld (%ld)", ucx_task_id(), in, item);
 		in = (in + 1) % N;
-		ucx_signal(mutex);
-		ucx_signal(full);
+		ucx_sem_signal(mutex);
+		ucx_sem_signal(full);
 	}
 }
 
@@ -26,13 +26,13 @@ void consumer(void)
 	int32_t item;
 
 	for (;;) {
-		ucx_wait(full);
-		ucx_wait(mutex);
+		ucx_sem_wait(full);
+		ucx_sem_wait(mutex);
 		item = buffer[out];
 		printf("\nconsumer %d getting from %ld (%ld)", ucx_task_id(), out, item);
 		out = (out + 1) % N;
-		ucx_signal(mutex);
-		ucx_signal(empty);
+		ucx_sem_signal(mutex);
+		ucx_sem_signal(empty);
 	}
 }
 
@@ -42,9 +42,9 @@ int32_t app_main(void)
 	ucx_task_add(consumer, DEFAULT_STACK_SIZE);
 	ucx_task_add(consumer, DEFAULT_STACK_SIZE);
 
-	empty = ucx_semcreate(N);
-	full = ucx_semcreate(0);
-	mutex = ucx_semcreate(1);
+	empty = ucx_sem_create(3, N);
+	full = ucx_sem_create(3, 0);
+	mutex = ucx_sem_create(3, 1);
 	
 	return 1;
 }
