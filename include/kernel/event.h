3d623ced7d2ece858916ce48ed407d4289e54455
sergio johann filho <sergio.johann@acad.pucrs.br>
2023-11-03 00:33:53 -0300

    add preliminary event queue implementation

--- include/kernel/event.h
+++ include/kernel/event.h
@@ -1,3 +1,19 @@
-int32_t ucx_event_queue(uint16_t events);
-int32_t ucx_event_add(void *callback, uint16_t delay);
-int32_t ucx_event_remove(void *callback);
+#define EQ_SEM_MAX_TASKS	16
+
+struct event_s {
+	void *(*callback)(void *);
+	void *data;
+	uint16_t type;
+};
+
+struct eq_s {
+	struct queue_s *event_queue;
+	struct sem_s *mutex;
+};
+
+struct eq_s *ucx_eq_create(uint16_t events);
+int32_t ucx_eq_destroy(struct eq_s *eq);
+int32_t ucx_event_post(struct eq_s *eq, struct event_s *e);
+int32_t ucx_event_poll(struct eq_s *eq);
+struct event_s *ucx_event_get(struct eq_s *eq);
+void *ucx_event_dispatch(struct event_s *e, void *arg);
--- kernel/event.c
+++ kernel/event.c
@@ -1,23 +1,82 @@
 /* file:          event.c
  * description:   event management
- * date:          04/2023
+ * date:          11/2023
  * author:        Sergio Johann Filho <sergio.johann@acad.pucrs.br>
  */
 
 #include <ucx.h>
 
-int32_t ucx_event_queue(uint16_t events)
+struct eq_s *ucx_eq_create(uint16_t events)
 {
-	// if event queue not created, do it
-	// if events not changed but this API is called, leave it alone
-	// if events is changed, recreate it (drop all events)
-	// if events is changed to zero, destroy queue
+	struct eq_s *eqptr;
+	
+	eqptr = malloc(sizeof(struct eq_s));
+	
+	if (!eqptr)
+		return 0;
+		
+	eqptr->event_queue = queue_create(events);
+	
+	if (!eqptr->event_queue) {
+		free(eqptr);
+		return 0;
+	}
+	
+	eqptr->mutex = ucx_sem_create(EQ_SEM_MAX_TASKS, 1);
+	
+	if (!eqptr->mutex) {
+		queue_destroy(eqptr->event_queue);
+		free(eqptr);
+		return 0;
+	}
+	
+	return eqptr;
 }
 
-int32_t ucx_event_add(void *callback, uint16_t delay)
+int32_t ucx_eq_destroy(struct eq_s *eq)
 {
+//	ucx_sem_wait(eq->mutex);
+	
+	return 0;
 }
 
-int32_t ucx_event_remove(void *callback)
+int32_t ucx_event_post(struct eq_s *eq, struct event_s *e)
 {
+	int32_t status;
+	
+	ucx_sem_wait(eq->mutex);
+	status = queue_enqueue(eq->event_queue, e);
+	ucx_sem_signal(eq->mutex);
+	
+	return status;
+}
+
+int32_t ucx_event_poll(struct eq_s *eq)
+{
+	int32_t ecount;
+	
+	ucx_sem_wait(eq->mutex);
+	ecount = queue_count(eq->event_queue);
+	ucx_sem_signal(eq->mutex);
+	
+	return ecount;
+} 
+
+struct event_s *ucx_event_get(struct eq_s *eq)
+{
+	struct event_s *e;
+	
+	ucx_sem_wait(eq->mutex);
+	e = queue_dequeue(eq->event_queue);
+	ucx_sem_signal(eq->mutex);
+	
+	return e;
+}
+
+void *ucx_event_dispatch(struct event_s *e, void *arg)
+{
+	if (e->callback)
+		return e->callback(arg);
+	else
+		return 0;
 }
