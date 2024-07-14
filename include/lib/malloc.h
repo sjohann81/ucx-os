edf0526fb028e84209893c3b8a9fa3232997ef4d
Sérgio Johann Filho <sergio.johann@acad.pucrs.br>
2024-07-13 21:24:15 -0300

    remove reference to heap pool

--- include/lib/malloc.h
+++ include/lib/malloc.h
@@ -11,6 +11,3 @@ void ucx_heap_init(size_t *zone, uint32_t len);
 void *ucx_calloc(uint32_t size, uint32_t type_size);
 void *ucx_realloc(void *ptr, uint32_t size);
 
-#ifdef UCX_OS_HEAP_SIZE
-extern char _heap[UCX_OS_HEAP_SIZE];
-#endif
