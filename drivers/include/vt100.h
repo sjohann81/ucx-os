d4ac6d69cec43b9c5630c6816851c6581542b553
Sérgio Johann Filho <sergio.johann@acad.pucrs.br>
2024-07-21 17:12:45 -0300

    added experimental vt100 driver

---
+++ drivers/include/vt100.h
@@ -0,0 +1,95 @@
+/* vt100 terminal device API */
+int vt100_term(const struct device_s *dev, uint8_t cmd);
+int vt100_textattr(const struct device_s *dev, uint8_t attr);
+int vt100_cursor(const struct device_s *dev, uint8_t cmd);
+int vt100_cursormove(const struct device_s *dev, uint8_t cmd, uint8_t cnt);
+int vt100_cursorpos(const struct device_s *dev, uint8_t lin, uint8_t col);
+int vt100_scrollrows(const struct device_s *dev, uint8_t rstart, uint8_t rend);
+int vt100_edit(const struct device_s *dev, uint8_t cmd);
+
+extern struct vt100_api_s vt100_api;
+
+enum terminal_cmd {
+	TERM_RESET,
+	TERM_SOFTRESET,
+	TERM_NEWLINE,
+	TERM_LINEFEED,
+	TERM_132CHARS,
+	TERM_80CHARS,
+	TERM_SMOOTHSCROLL,
+	TERM_JUMPSCROLL,
+	TERM_INVERSEVID,
+	TERM_NORMALVID,
+	TERM_LINEWRAP,
+	TERM_NOLINEWRAP,
+	TERM_DISPLAYON,
+	TERM_DISPLAYOFF
+};
+
+enum text_attr {
+	TEXT_RESET		= 0,
+	TEXT_BOLD		= 1,
+	TEXT_DIM		= 2,
+	TEXT_UNDERLINE		= 4,
+	TEXT_BLINK		= 5,
+	TEXT_INVERSE		= 7,
+	TEXT_HIDDEN		= 8,
+
+	TEXT_BLACK		= 30,
+	TEXT_RED		= 31,
+	TEXT_GREEN		= 32,
+	TEXT_YELLOW		= 33,
+	TEXT_BLUE		= 34,
+	TEXT_MAGENTA		= 35,
+	TEXT_CYAN		= 36,
+	TEXT_WHITE		= 37,
+	TEXT_DEFAULT		= 39,
+	
+	TEXT_BG_BLACK		= 40,
+	TEXT_BG_RED		= 41,
+	TEXT_BG_GREEN		= 42,
+	TEXT_BG_YELLOW		= 43,
+	TEXT_BG_BLUE		= 44,
+	TEXT_BG_MAGENTA		= 45,
+	TEXT_BG_CYAN		= 46,
+	TEXT_BG_WHITE		= 47,
+	TEXT_BG_DEFAULT		= 49
+};
+
+enum cursor_cmd {
+	CURSOR_ON,
+	CURSOR_OFF,
+	CURSOR_BLINK,
+	CURSOR_STATIC,
+	CURSOR_HOME,
+	CURSOR_UP,
+	CURSOR_DOWN,
+	CURSOR_RIGHT,
+	CURSOR_LEFT,
+	CURSOR_UP_N,
+	CURSOR_DOWN_N,
+	CURSOR_RIGHT_N,
+	CURSOR_LEFT_N,
+	CURSOR_SAVE,
+	CURSOR_RESTORE,
+	SCROLL_SCREEN,
+	SCROLL_UP,
+	SCROLL_DOWN,
+	TAB_SET,
+	TAB_CLEAR,
+	TAB_CLEAR_ALL
+};
+
+enum edit_cmd {
+	MODE_INSERT,
+	MODE_REPLACE,
+	ERASE_CHAR_RIGHT,
+	ERASE_CHAR_LEFT,
+	ERASE_EOL,
+	ERASE_SOL,
+	ERASE_LINE,
+	ERASE_CLEAR_DOWN,
+	ERASE_CLEAR_UP,
+	ERASE_CLEAR_SCREEN,
+	INSERT_LINE
+};
---
+++ drivers/vt100/vt100.c
@@ -0,0 +1,205 @@
+#include <ucx.h>
+#include <vt100.h>
+
+struct vt100_api_s {
+	int (*term)(const struct device_s *dev, uint8_t cmd);
+	int (*textattr)(const struct device_s *dev, uint8_t attr);
+	int (*cursor)(const struct device_s *dev, uint8_t cmd);
+	int (*cursormove)(const struct device_s *dev, uint8_t cmd, uint8_t cnt);
+	int (*cursorpos)(const struct device_s *dev, uint8_t lin, uint8_t col);
+	int (*scrollrows)(const struct device_s *dev, uint8_t rstart, uint8_t rend);
+	int (*edit)(const struct device_s *dev, uint8_t cmd);
+};
+
+/* vt100 terminal device API function wrappers */
+int vt100_term(const struct device_s *dev, uint8_t cmd)
+{
+	struct vt100_api_s *api;
+
+	api = (struct vt100_api_s *)dev->custom_api;
+	return api->term(dev, cmd);
+}
+
+int vt100_textattr(const struct device_s *dev, uint8_t attr)
+{
+	struct vt100_api_s *api;
+
+	api = (struct vt100_api_s *)dev->custom_api;
+	return api->textattr(dev, attr);
+}
+
+int vt100_cursor(const struct device_s *dev, uint8_t cmd)
+{
+	struct vt100_api_s *api;
+
+	api = (struct vt100_api_s *)dev->custom_api;
+	return api->cursor(dev, cmd);
+}
+
+int vt100_cursormove(const struct device_s *dev, uint8_t cmd, uint8_t cnt)
+{
+	struct vt100_api_s *api;
+
+	api = (struct vt100_api_s *)dev->custom_api;
+	return api->cursormove(dev, cmd, cnt);
+}
+
+int vt100_cursorpos(const struct device_s *dev, uint8_t lin, uint8_t col)
+{
+	struct vt100_api_s *api;
+
+	api = (struct vt100_api_s *)dev->custom_api;
+	return api->cursorpos(dev, lin, col);
+}
+
+int vt100_scrollrows(const struct device_s *dev, uint8_t rstart, uint8_t rend)
+{
+	struct vt100_api_s *api;
+
+	api = (struct vt100_api_s *)dev->custom_api;
+	return api->scrollrows(dev, rstart, rend);
+}
+
+int vt100_edit(const struct device_s *dev, uint8_t cmd)
+{
+	struct vt100_api_s *api;
+
+	api = (struct vt100_api_s *)dev->custom_api;
+	return api->edit(dev, cmd);
+}
+
+/* vt100 device driver implementation */
+static int driver_term(const struct device_s *dev, uint8_t cmd)
+{
+	int retval = 0;
+	
+	NOSCHED_ENTER();
+	switch (cmd) {
+	case TERM_RESET:		printf("\033c"); break;
+	case TERM_SOFTRESET:		printf("\033[!p"); break;
+	case TERM_NEWLINE:		printf("\033[20h"); break;
+	case TERM_LINEFEED:		printf("\033[20l"); break;
+	case TERM_132CHARS:		printf("\033[?3h"); break;
+	case TERM_80CHARS:		printf("\033[?3l"); break;
+	case TERM_SMOOTHSCROLL:		printf("\033[?4h"); break;
+	case TERM_JUMPSCROLL:		printf("\033[?4l"); break;
+	case TERM_INVERSEVID:		printf("\033[?5h"); break;
+	case TERM_NORMALVID:		printf("\033[?5l"); break;
+	case TERM_LINEWRAP:		printf("\033[?7h"); break;
+	case TERM_NOLINEWRAP:		printf("\033[?7l"); break;
+	case TERM_DISPLAYON:		printf("\033[?75h"); break;
+	case TERM_DISPLAYOFF:		printf("\033[?75l"); break;
+	default: retval = -1;
+	}
+	NOSCHED_LEAVE();
+	
+	return retval;
+}
+
+static int driver_textattr(const struct device_s *dev, uint8_t attr)
+{
+	NOSCHED_ENTER();
+	printf("\033[%dm", attr);
+	NOSCHED_LEAVE();
+	
+	return 0;
+}
+
+static int driver_cursor(const struct device_s *dev, uint8_t cmd)
+{
+	int retval = 0;
+	
+	NOSCHED_ENTER();
+	switch (cmd) {
+	case CURSOR_ON:			printf("\033[?25h"); break;
+	case CURSOR_OFF:		printf("\033[?25l"); break;
+	case CURSOR_BLINK:		printf("\033[?12h"); break;
+	case CURSOR_STATIC:		printf("\033[?12l"); break;
+	case CURSOR_HOME:		printf("\033[H"); break;
+	case CURSOR_UP:			printf("\033[A"); break;
+	case CURSOR_DOWN:		printf("\033[B"); break;
+	case CURSOR_RIGHT:		printf("\033[C"); break;
+	case CURSOR_LEFT:		printf("\033[D"); break;
+	case CURSOR_SAVE:		printf("\0337"); break;
+	case CURSOR_RESTORE:		printf("\0338"); break;
+	case SCROLL_SCREEN:		printf("\033[r"); break;
+	case SCROLL_UP:			printf("\033D"); break;
+	case SCROLL_DOWN:		printf("\033M"); break;
+	case TAB_SET:			printf("\033H"); break;
+	case TAB_CLEAR:			printf("\033[g"); break;
+	case TAB_CLEAR_ALL:		printf("\033[3g"); break;
+	default: retval = -1;
+	}
+	NOSCHED_LEAVE();
+	
+	return retval;
+}
+
+static int driver_cursormove(const struct device_s *dev, uint8_t cmd, uint8_t cnt)
+{
+	int retval = 0;
+	
+	NOSCHED_ENTER();
+	switch (cmd) {
+	case CURSOR_UP_N:		printf("\033[%dA", cnt); break;
+	case CURSOR_DOWN_N:		printf("\033[%dB", cnt); break;
+	case CURSOR_RIGHT_N:		printf("\033[%dC", cnt); break;
+	case CURSOR_LEFT_N:		printf("\033[%dD", cnt); break;
+	default: retval = -1;
+	}
+	NOSCHED_LEAVE();
+	
+	return retval;
+}
+
+static int driver_cursorpos(const struct device_s *dev, uint8_t lin, uint8_t col)
+{
+	NOSCHED_ENTER();
+	printf("\033[%d;%dH", lin, col);
+	NOSCHED_LEAVE();
+	
+	return 0;
+}
+
+static int driver_scrollrows(const struct device_s *dev, uint8_t rstart, uint8_t rend)
+{
+	NOSCHED_ENTER();
+	printf("\033[%d;%dr", rstart, rend);
+	NOSCHED_LEAVE();
+	
+	return 0;
+}
+
+static int driver_edit(const struct device_s *dev, uint8_t cmd)
+{
+	int retval = 0;
+	
+	NOSCHED_ENTER();
+	switch (cmd) {
+	case MODE_INSERT:		printf("\033[4h"); break;
+	case MODE_REPLACE:		printf("\033[4l"); break;
+	case ERASE_CHAR_RIGHT:		printf("\033[P"); break;
+	case ERASE_CHAR_LEFT:		printf("\033[M"); break;
+	case ERASE_EOL:			printf("\033[K"); break;
+	case ERASE_SOL:			printf("\033[1K"); break;
+	case ERASE_LINE:		printf("\033[2K"); break;
+	case ERASE_CLEAR_DOWN:		printf("\033[J"); break;
+	case ERASE_CLEAR_UP:		printf("\033[1J"); break;
+	case ERASE_CLEAR_SCREEN:	printf("\033[2J"); break;
+	case INSERT_LINE:		printf("\033[L"); break;
+	default: retval = -1;
+	}
+	NOSCHED_LEAVE();
+	
+	return retval;
+}
+
+struct vt100_api_s vt100_api = {
+	.term = driver_term,
+	.textattr = driver_textattr,
+	.cursor = driver_cursor,
+	.cursormove = driver_cursormove,
+	.cursorpos = driver_cursorpos,
+	.scrollrows = driver_scrollrows,
+	.edit = driver_edit
+};
