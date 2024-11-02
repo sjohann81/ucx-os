b5ad50be0b853e0f0905d523280e9c9ba702a99f
Sérgio Johann Filho <sergio.johann@acad.pucrs.br>
2024-11-02 15:36:54 -0300

    update ATMEGA328p uart sources

--- arch/avr/atmega328p/usart.h
+++ arch/avr/atmega328p/usart.h
@@ -1,5 +1,5 @@
-void uart_init(uint32_t baud, uint8_t polled);
-void uart_flush(void);
-uint16_t uart_rxsize(void);
-void uart_tx(uint8_t data);
-uint8_t uart_rx(void);
+void usart_init(uint32_t baud, uint8_t polled);
+void usart_flush(void);
+uint16_t usart_rxsize(void);
+void usart_tx(uint8_t data);
+uint8_t usart_rx(void);
