9f3d020c2b2b99e4462799516b6b2347ad036b15
Sérgio Johann Filho <sergio.johann@acad.pucrs.br>
2024-12-22 20:34:57 -0300

    add ioctl() function pointer to generic device interface

--- drivers/include/device.h
+++ drivers/include/device.h
@@ -19,4 +19,5 @@ struct device_api_s {
 	int (*dev_close)(const struct device_s *dev);
 	size_t (*dev_read)(const struct device_s *dev, void *buf, size_t count);
 	size_t (*dev_write)(const struct device_s *dev, void *buf, size_t count);
+	int (*dev_ioctl)(const struct device_s *dev, int cmd, void *data);
 };
