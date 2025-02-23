7a3e6c0606ebaee041a9be98f0dd38c3f0765bfe
Sérgio Johann Filho <sergio.johann@acad.pucrs.br>
2025-02-23 17:48:27 -0300

    first test of generic gpio driver

---
+++ drivers/gpio/gpio.c
@@ -0,0 +1,124 @@
+#include <ucx.h>
+#include <device.h>
+#include <gpio.h>
+
+/* GPIO API function wrappers */
+int gpio_config(const struct device_s *dev)
+{
+	struct gpio_api_s *api;
+
+	api = (struct gpio_api_s *)dev->custom_api;
+	return api->gpio_config(dev);
+}
+	
+uint16_t gpio_get(const struct device_s *dev)
+{
+	struct gpio_api_s *api;
+
+	api = (struct gpio_api_s *)dev->custom_api;
+	return api->gpio_get(dev);
+}
+
+void gpio_set(const struct device_s *dev, uint16_t pins)
+{
+	struct gpio_api_s *api;
+
+	api = (struct gpio_api_s *)dev->custom_api;
+	api->gpio_set(dev, (int)pins);
+}
+
+void gpio_setbits(const struct device_s *dev, uint16_t pins)
+{
+	struct gpio_api_s *api;
+
+	api = (struct gpio_api_s *)dev->custom_api;
+	api->gpio_setbits(dev, (int)pins);
+}
+
+void gpio_clearbits(const struct device_s *dev, uint16_t pins)
+{
+	struct gpio_api_s *api;
+
+	api = (struct gpio_api_s *)dev->custom_api;
+	api->gpio_clearbits(dev, (int)pins);
+}
+
+void gpio_togglebits(const struct device_s *dev, uint16_t pins)
+{
+	struct gpio_api_s *api;
+
+	api = (struct gpio_api_s *)dev->custom_api;
+	api->gpio_togglebits(dev, (int)pins);
+}
+
+
+/* GPIO device driver implementation */
+static int driver_config(const struct device_s *dev)
+{
+	struct gpio_config_s *config;
+	int val;
+	
+	config = (struct gpio_config_s *)dev->config;
+	
+	val = config->gpio_ll_config(&config->config_values);
+	
+	return val;
+}
+
+static int driver_get(const struct device_s *dev)
+{
+	struct gpio_config_s *config;
+	int val;
+	
+	config = (struct gpio_config_s *)dev->config;
+	
+	val = config->gpio_ll_get(&config->config_values);
+	
+	if (val < 0)
+		return val;
+	else
+		return val & 0xffff;
+}
+
+static void driver_set(const struct device_s *dev, int pins)
+{
+	struct gpio_config_s *config;
+	
+	config = (struct gpio_config_s *)dev->config;
+	
+	config->gpio_ll_set(&config->config_values, pins);
+}
+
+static void driver_setbits(const struct device_s *dev, int pins)
+{
+	uint16_t val;
+	
+	val = driver_get(dev) | pins;
+	driver_set(dev, val);
+}
+
+static void driver_clearbits(const struct device_s *dev, int pins)
+{
+	uint16_t val;
+	
+	val = driver_get(dev) & ~pins;
+	driver_set(dev, val);
+}
+
+static void driver_togglebits(const struct device_s *dev, int pins)
+{
+	uint16_t val;
+	
+	val = driver_get(dev) ^ pins;
+	driver_set(dev, val);
+}
+
+/* GPIO device driver function mapping */
+struct gpio_api_s gpio_api = {
+	.gpio_config = driver_config,
+	.gpio_get = driver_get,
+	.gpio_set = driver_set,
+	.gpio_setbits = driver_setbits,
+	.gpio_clearbits = driver_clearbits,
+	.gpio_togglebits = driver_togglebits
+};
--- drivers/include/gpio.h
+++ drivers/include/gpio.h
@@ -1,14 +1,41 @@
-void gpio_init(const struct device_s *dev, uint8_t port);
-void gpio_deinit(const struct device_s *dev);
-void gpio_setup(const struct device_s *dev, struct gpio_config_values_s *new_config);
-int gpio_get(const struct device_s *dev, uint16_t pin);
-void gpio_set(const struct device_s *dev, uint16_t pin);
-void gpio_clear(const struct device_s *dev, uint16_t pin);
-void gpio_toggle(const struct device_s *dev, uint16_t pin);
-uint16_t gpio_read(const struct device_s *dev);
-void gpio_write(const struct device_s *dev, uint16_t pins);
+/* GPIO driver API */
+int gpio_config(const struct device_s *dev);
+uint16_t gpio_get(const struct device_s *dev);
+void gpio_set(const struct device_s *dev, uint16_t pins);
+void gpio_setbits(const struct device_s *dev, uint16_t pins);
+void gpio_clearbits(const struct device_s *dev, uint16_t pins);
+void gpio_togglebits(const struct device_s *dev, uint16_t pins);
+
+/* GPIO configuration definitions */
+struct gpio_api_s {
+	int (*gpio_config)(const struct device_s *dev);
+	int (*gpio_get)(const struct device_s *dev);
+	void (*gpio_set)(const struct device_s *dev, int val);
+	void (*gpio_setbits)(const struct device_s *dev, int val);
+	void (*gpio_clearbits)(const struct device_s *dev, int val);
+	void (*gpio_togglebits)(const struct device_s *dev, int val);
+};
+
+enum port_names {
+	GPIO_PORTA, GPIO_PORTB, GPIO_PORTC, GPIO_PORTD,
+	GPIO_PORTE, GPIO_PORTF, GPIO_PORTG, GPIO_PORTH,
+	GPIO_PORTI, GPIO_PORTJ, GPIO_PORTK, GPIO_PORTL,
+	GPIO_PORTM, GPIO_PORTN, GPIO_PORTO, GPIO_PORTP
+};
+
+enum pin_names {
+	GPIO_PIN0 = (1 << 0), GPIO_PIN1 = (1 << 1),
+	GPIO_PIN2 = (1 << 2), GPIO_PIN3 = (1 << 3),
+	GPIO_PIN4 = (1 << 4), GPIO_PIN5 = (1 << 5),
+	GPIO_PIN6 = (1 << 6), GPIO_PIN7 = (1 << 7),
+	GPIO_PIN8 = (1 << 8), GPIO_PIN9 = (1 << 9),
+	GPIO_PIN10 = (1 << 10), GPIO_PIN11 = (1 << 11),
+	GPIO_PIN12 = (1 << 12), GPIO_PIN13 = (1 << 13),
+	GPIO_PIN14 = (1 << 14), GPIO_PIN15 = (1 << 15)
+};
 
 struct gpio_config_values_s {
+	uint8_t port;
 	uint16_t pin;
 	uint16_t direction;
 	uint16_t pull;
@@ -16,11 +43,10 @@ struct gpio_config_values_s {
 };
 
 struct gpio_config_s {
-	int (*gpio_init)(uint8_t port);
-	int (*gpio_deinit)(uint8_t port);
-	int (*gpio_config)(struct gpio_config_values_s *config);
-	int (*gpio_get)(void);
-	int (*gpio_set)(int val);
-	int (*gpio_read)(void);
-	int (*gpio_write)(int val);
+	struct gpio_config_values_s config_values;
+	int (*gpio_ll_config)(struct gpio_config_values_s *cfg);
+	int (*gpio_ll_get)(struct gpio_config_values_s *cfg);
+	int (*gpio_ll_set)(struct gpio_config_values_s *cfg, int val);
 };
+
+extern struct gpio_api_s gpio_api;
