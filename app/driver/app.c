146093117382df0cdc8d73025f80df7fc62e26e1
Sérgio Johann Filho <sergio.johann@acad.pucrs.br>
2025-03-07 16:00:59 -0300

    update example

--- app/driver/app.c
+++ app/driver/app.c
@@ -5,13 +5,13 @@
 struct my_data_s my_data1, my_data2;
 
 /* device driver instantiation */
-struct device_s device1 = {
+const struct device_s device1 = {
 	.name = "device 1",
 	.data = &my_data1,
 	.api = &dev_api
 };
 
-struct device_s device2 = {
+const struct device_s device2 = {
 	.name = "device 2",
 	.data = &my_data2,
 	.api = &dev_api
@@ -20,52 +20,64 @@ struct device_s device2 = {
 /* user tasks */
 void task0(void)
 {
-	struct device_s *dev = &device1;
+	const struct device_s *dev = &device1;
 	char buf[BUF_SIZE];
 	int i = 0;
 	
 	while (1) {
-		if (dev->api->dev_open(dev, 0))
-			ucx_task_yield();
-		
+		if (dev->api->dev_open(dev, 0)) {
+			ucx_task_delay(MS_TO_TICKS(100));
+			continue;
+		}
 		sprintf(buf, "hello world! %d", i++);
 		dev->api->dev_write(dev, buf, strlen(buf) + 1);
 		dev->api->dev_close(dev);
 		
-		ucx_task_yield();
+		ucx_task_delay(MS_TO_TICKS(1000));
 	}
 }
 
 void task1(void)
 {
-	struct device_s *dev = &device1;
+	const struct device_s *dev = &device1;
 	char buf[BUF_SIZE];
 	size_t size;
 	
 	while (1) {
-		if (dev->api->dev_open(dev, 0))
-			ucx_task_yield();
+		if (dev->api->dev_open(dev, 0)) {
+			ucx_task_delay(MS_TO_TICKS(100));
+			continue;
+		}
 
 		size = dev->api->dev_read(dev, buf, BUF_SIZE);
 		if (size)
 			printf("data (%d): %s\n", size, buf);
 		dev->api->dev_close(dev);
-		ucx_task_yield();
+		
+		ucx_task_delay(MS_TO_TICKS(1000));
 	}
 }
+
+void idle(void)
+{
+	while (1) {
+	}
+}
+
 
 /* application entry point */
 int32_t app_main(void)
 {
-	struct device_s *dev1 = &device1;
-	struct device_s *dev2 = &device2;
+	const struct device_s *dev1 = &device1;
+	const struct device_s *dev2 = &device2;
 	
 	ucx_task_spawn(task0, DEFAULT_STACK_SIZE);
 	ucx_task_spawn(task1, DEFAULT_STACK_SIZE);
+	ucx_task_spawn(idle, DEFAULT_STACK_SIZE);
 	ucx_task_priority(0, TASK_LOW_PRIO);
 	dev1->api->dev_init(dev1);
 	dev2->api->dev_init(dev2);
 
-	// start UCX/OS, cooperative mode
-	return 0;
+	// start UCX/OS
+	return 1;
 }
--- app/driver/driver.c
+++ app/driver/driver.c
@@ -9,10 +9,12 @@ static int my_init(const struct device_s *dev)
 	
 	printf("DEV: device init\n");
 	
+	CRITICAL_ENTER();
 	pdata = (struct my_data_s *)dev->data;
 	memset(pdata->buff, 0, BUF_SIZE);
 	pdata->size = 0;
 	pdata->in_use = -1;
+	CRITICAL_LEAVE();
 
 	return 0;
 }
@@ -20,48 +22,57 @@ static int my_init(const struct device_s *dev)
 static int my_open(const struct device_s *dev, int mode)
 {
 	struct my_data_s *pdata;
-	
+	int retval = 0;
+
+	CRITICAL_ENTER();	
 	pdata = (struct my_data_s *)dev->data;
 
 	if (pdata->in_use == -1) {
 		pdata->in_use = ucx_task_id();
-		printf("DEV: device open (task %d)\n", ucx_task_id());
+		printf("DEV: device open (task %d)\n", pdata->in_use);
 	} else {
 		printf("DEV: device open failed\n");
 
-		return -1;
+		retval = -1;
 	}
 	CRITICAL_LEAVE();
 
-	return 0;
+	return retval;
 }
 
 static int my_close(const struct device_s *dev)
 {
 	struct my_data_s *pdata;
-	
+	int retval = 0;
+
+	CRITICAL_ENTER();	
 	pdata = (struct my_data_s *)dev->data;
 
 	if (pdata->in_use > -1) {
+		printf("DEV: device close (task %d)\n", pdata->in_use);
 		pdata->in_use = -1;
-		printf("DEV: device close (task %d)\n", ucx_task_id());
 	} else {
 		printf("DEV: device close failed\n");
-		CRITICAL_LEAVE();
 		
-		return -1;
+		retval = -1;
 	}
+	CRITICAL_LEAVE();
 
-	return 0;
+	return retval;
 }
 
 static size_t my_read(const struct device_s *dev, void *buf, size_t count)
 {
 	struct my_data_s *pdata;
 	
+	CRITICAL_ENTER();
 	pdata = (struct my_data_s *)dev->data;
-	if (pdata->in_use != ucx_task_id())
+	
+	if (pdata->in_use != ucx_task_id()) {
+		CRITICAL_LEAVE();
+		
 		return -1;
+	}
 
 	if (count < pdata->size)
 		pdata->size = count;
@@ -70,6 +81,7 @@ static size_t my_read(const struct device_s *dev, void *buf, size_t count)
 		
 	memcpy(buf, pdata->buff, pdata->size);
 	pdata->size = 0;
+	CRITICAL_LEAVE();
 	
 	printf("DEV: device read %d bytes\n", count);
 	
@@ -79,13 +91,19 @@ static size_t my_read(const struct device_s *dev, void *buf, size_t count)
 static size_t my_write(const struct device_s *dev, void *buf, size_t count)
 {
 	struct my_data_s *pdata;
-	
+
+	CRITICAL_ENTER();
 	pdata = (struct my_data_s *)dev->data;
-	if (pdata->in_use != ucx_task_id())
+	
+	if (pdata->in_use != ucx_task_id()) {
+		CRITICAL_LEAVE();
+		
 		return -1;
+	}
 
 	memcpy(pdata->buff, buf, count);
 	pdata->size = count;
+	CRITICAL_LEAVE();
 	
 	printf("DEV: device write %d bytes\n", count);
 	
