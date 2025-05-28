cc4281e097ef3b5c79f4498df4d7d15b8bd8fa87
VictorGilbert <zevictos@gmail.com>
2025-05-28 09:53:06 -0400

    fix include were not to the right files and was missing struct space definition

--- arch/arm/armv7/source/uaccess.c
+++ arch/arm/armv7/source/uaccess.c
@@ -13,10 +13,72 @@
  * See the GNU General Public License version 3 for more details.
  */
 
-#include <asp/uaccess.h>
-#include <assert.h>
-#include <arch/space.h>
-#include <arch/deployment.h>
+// #include <asp/uaccess.h>
+#include <libpok_legacy/assert.h>
+#include <libpok_legacy/space.h>
+// #include <libpok_legacy/deployment.h>
+struct ja_armv7_space
+{
+    /*
+     * Physical address of memory chunk.
+     *
+     * If it is set to 0 in deployment.c, it will be filled upon space initialization.
+     */
+    uint32_t   phys_base;
+
+    /*
+     * Size of the memory  (code and static data).
+     *
+     * Set in deployment.c.
+     */
+    size_t      size_normal;
+
+    /*
+     * Size of the memory  (heap).
+     *
+     * Set in deployment.c.
+     */
+    size_t      size_heap;
+
+    /*
+     * Size of the memory for stacks.
+     *
+     * Set in deployment.c.
+     */
+    size_t size_stack;
+
+    /*
+     * Total size for partition's use.
+     *
+     * Calculated upon space initialization.
+     */
+    size_t size_total;
+
+    /*
+     * Offset from the beginning of the space to the ending of the heap.
+     *
+     * Calculated upon space initialization.
+     */
+    size_t heap_end;
+    /*
+     * Memory currently used for stacks.
+     */
+    size_t size_stack_used;
+
+    uint32_t mpu_base[11];
+    uint32_t mpu_size[11];
+    uint32_t mpu_reg;
+    uint32_t mpu_type;
+    uint32_t mpu_perm;
+};
+
+/*
+ * Array of user space descriptions.
+ *
+ * Should be defined in deployment.c.
+ */
+extern struct ja_armv7_space ja_spaces[];
+extern int ja_spaces_n;
 
 // TODO: Revisit (see also pok_space_* code in arch.h)
 static pok_bool_t arm_check_access(const void* __user addr, size_t size,
