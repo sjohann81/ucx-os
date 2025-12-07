fc344360e52dce3995ab1ef06c5cab17fb484441
Sérgio Johann Filho <sergio.johann@acad.pucrs.br>
2025-12-07 10:49:26 -0300

    add messages_multicore.c example

---
+++ app/messages_multicore.c
@@ -0,0 +1,140 @@
+#include <ucx.h>
+#include <ieee754.h>
+
+/* application tasks */
+
+enum {TYPE_STRING, TYPE_INT, TYPE_FLOAT};
+
+struct mq_s *mq1, *mq2;
+
+void malloc_failed()
+{
+	printf("malloc() failed!\n");
+	ucx_task_cancel(ucx_task_id());
+}
+
+void task1(void)
+{
+	int val = 0;
+	float fval = 0.0f;
+	//char str[50], int_str[20], float_str[20];
+	char *str;
+	struct message_s msg1, msg2, msg3;
+	struct message_s *pmsg;
+	
+	while (1) {
+		str = malloc(50);
+		printf("malloc(): %p\n", str);
+		if (!str) malloc_failed();
+		
+		sprintf(str, "hello %d from t1...", val);
+		pmsg = &msg1;
+		pmsg->data = (void *)str;
+		ucx_mq_enqueue(mq1, pmsg);
+
+		str = malloc(20);
+		printf("malloc(): %p\n", str);
+		if (!str) malloc_failed();
+		
+		val++;
+		pmsg = &msg2;
+		itoa(val, str, 10);
+		pmsg->data = (void *)str;
+		pmsg->type = TYPE_INT;
+		ucx_mq_enqueue(mq2, pmsg);
+		
+		str = malloc(20);
+		printf("malloc(): %p\n", str);
+		if (!str) malloc_failed();
+		
+		fval += 0.123;
+		pmsg = &msg3;
+		ftoa(fval, str, 6);
+		pmsg->data = (void *)str;
+		pmsg->type = TYPE_FLOAT;
+		ucx_mq_enqueue(mq2, pmsg);
+		
+		ucx_task_delay(10);
+	}
+}
+
+void task2(void)
+{
+	struct message_s *pmsg;
+	char *str;
+	
+	while (1) {
+		if (ucx_mq_items(mq1) > 0) {
+			pmsg = ucx_mq_dequeue(mq1);
+			if (pmsg) {
+				str = pmsg->data;
+				printf("%s\n", str);
+				free(str);
+			}
+		} else {
+			ucx_task_wfi();
+		}
+	}
+}
+
+void task3(void)
+{
+	struct message_s *pmsg;
+	char *str;
+	int val;
+	float fval;
+	char str2[50];
+	
+	while (1) {
+		if (ucx_mq_items(mq2) > 0) {
+			pmsg = ucx_mq_dequeue(mq2);
+			if (pmsg) {
+				str = pmsg->data;
+				
+				switch (pmsg->type) {
+				case TYPE_INT:
+					val = atoi(str);
+					itoa(val, str2, 10);
+					break;
+				case TYPE_FLOAT:
+					fval = atof(str);
+					ftoa(fval, str2, 6);
+					break;
+				default: break;
+				}
+				
+				printf("recv str: %s converted value: %s\n", str, str2);
+				free(str);
+			}
+		} else {
+			ucx_task_wfi();
+		}
+	}
+}
+
+void idle(void)
+{
+	while (1) {
+		ucx_task_wfi();
+	}
+}
+
+int app_main(void)
+{
+	ucx_task_spawn(idle, DEFAULT_STACK_SIZE);
+	ucx_task_spawn(task1, DEFAULT_STACK_SIZE);
+	ucx_task_spawn(task2, DEFAULT_STACK_SIZE);
+	
+	mq1 = ucx_mq_create(8);
+	mq2 = ucx_mq_create(8);
+	
+	return 1;
+}
+
+int app_main1(void)
+{
+	ucx_task_spawn(idle, DEFAULT_STACK_SIZE);
+	ucx_task_spawn(task3, DEFAULT_STACK_SIZE);
+	
+	return 1;
+}
